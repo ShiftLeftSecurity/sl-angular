07e28fcd058b67fe151b476ffaebf78af4e09e13
Prabhu Subramanian <prabhu@shiftleft.io>
2020-11-19 01:03:42 +0000

    added login/logout

--- src/app/modules/core/services/auth.service.ts
+++ src/app/modules/core/services/auth.service.ts
@@ -8,9 +8,37 @@ import { AppConfig } from '../../../configs/app.config';
 export class AuthService {
     constructor(private http: HttpClient) { }
     isAuthenticated() {
-        return this.http.get('/auth/isAuthenticated');
+        return this.http.get('/auth/isAuthenticated', {
+        	withCredentials: true,
+        	responseType: 'blob'
+        });
     }
     getAuthorizationToken() {
     	return AppConfig.authToken;
     }
+    login(username: string, password: string) {
+      const formData  = new FormData();
+      formData.append('username', username);
+      formData.append('password', password);
+      let options = {
+        headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+      };
+      return this.http.post<any>(`/login`, formData, options)
+        .pipe(map(user => {
+            if (user) {
+                console.log('aut successful')
+                // store user details in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                this.currentUserSubject.next(user);
+            }
+
+            return user;
+        }));
+	  }
+
+    logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('currentUser');
+    }
+
 }
---
+++ src/app/modules/core/services/check-update.ts
@@ -0,0 +1,17 @@
+import { ApplicationRef, Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { concat, interval } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+@Injectable()
+export class CheckForUpdateService {
+
+  constructor(appRef: ApplicationRef, updates: SwUpdate) {
+    // Allow the app to stabilize first, before starting polling for updates with `interval()`.
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+    const everySixMins$ = interval(6 * 60 * 1000);
+    const everySixMinsOnceAppIsStable$ = concat(appIsStable$, everySixMins$);
+
+    everySixMinsOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+  }
+}
